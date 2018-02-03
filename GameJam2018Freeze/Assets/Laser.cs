5bcdaf0668d8e122952a1858414bdb4add776673
Lucia <lucia.okeh@gmail.com>
2018-02-03 16:15:20 -0500

    fixing laser beam.. final for now

--- GameJam2018Freeze/Assets/Laser.cs
+++ GameJam2018Freeze/Assets/Laser.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Laser : MonoBehaviour {
 
 	bool laserOn = false;
+	bool firstLaser = true;
 	public Transform startPoint;
 	public Transform endPoint;
 	LineRenderer laser;
@@ -13,31 +14,61 @@ public class Laser : MonoBehaviour {
 	public float distance;
 
 	public float lineSpeed = 4f;
+	float laserTime = 8f;
 
 	// Use this for initialization
 	void Start () {
 		laser = GetComponent<LineRenderer> ();
-		laser.SetWidth (0.7f, 0.7f);
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		laser.SetPosition (0, startPoint.position);
-		laser.SetPosition (1, endPoint.position);
-		distance = Vector3.Distance (startPoint.position, endPoint.position);
+	void Update () { 
 
-		counter = counter + 0.1f / lineSpeed;
+		laserTime = laserTime - Time.deltaTime;
+		Debug.Log (laserTime);
 
-		float x = Mathf.Lerp (0, distance, counter);
+		if ((!laserOn && laserTime <= 0f) || firstLaser) {
 
-		Vector3 pointOnLine = x * Vector3.Normalize (endPoint.position - startPoint.position) + startPoint.position;
+			laserTime = 8;
+			laser.enabled = true;
+			turnLaserOn ();
 
-		laser.SetPosition (1,pointOnLine);
+			laser.SetPosition (0, startPoint.position);
+			laser.SetPosition (1, endPoint.position);
+			distance = Vector3.Distance (startPoint.position, endPoint.position);
+
+			counter = counter + 0.1f / lineSpeed;
+
+			float x = Mathf.Lerp (0, distance, counter);
+
+			Vector3 pointOnLine = endPoint.position;//x * Vector3.Normalize (endPoint.position - startPoint.position) + startPoint.position;
+
+			laser.SetPosition (1, pointOnLine);
+
+			firstLaser = false;
+
+		}
+
 	}
 
 	void turnLaserOn(){
 		laserOn = true;
+		StartCoroutine(Blink(4, 0.2f, 0.23f));
 	}
 
+	IEnumerator Blink(int nTimes, float timeOn, float timeOff){
+		yield return new WaitForSeconds(3f);
+
+		while (nTimes > 0){
+			laser.enabled = true;
+			yield return new WaitForSeconds(timeOn);
+			laser.enabled = false;
+			yield return new WaitForSeconds(timeOff);
+			nTimes--;
+		}
+		laserOn = false;
+
+	}
 
 }

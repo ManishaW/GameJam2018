fc123aae11d62d6c4083b132de2ee3cef82c828c
Manisha W <mwann031@uottawa.ca>
2018-02-03 15:05:42 -0500

    Manisha scrolling

---
+++ GameJam2018Freeze/Assets/scripts/cameraScroll.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScroll : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.touchCount>0) {
+		} else {
+			float step = 7;
+
+			var cameraPosition = Camera.main.gameObject.transform.position;
+			cameraPosition.x += step;
+			Camera.main.gameObject.transform.position = cameraPosition;
+		}
+
+		}
+
+
+}
